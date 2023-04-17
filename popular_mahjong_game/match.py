4c1bd36d2731a42dce6281a75ac5c37f9ec5bebb
Nranphy <3102002900@qq.com>
2023-04-18 00:22:43 +0800

    fix: 优化Match.discard逻辑，适配前端需求。

--- popular_mahjong_game/match.py
+++ popular_mahjong_game/match.py
@@ -322,7 +322,7 @@ class Match:
         player = self.player[player_index]
         if not (tile_type or discard_draw):
             raise DiscardException(f"切牌信息不足，切牌失败。")
-        elif discard_draw and player.draw:
+        elif discard_draw and player.draw==tile_type:
             player.discard.append((player.draw, False))
             tile_type = player.draw
         else:
